6c2f798c1223483ade79904eb04b2ad805d26e8e
Colin Parsons <colinparsons@Colins-MacBook-Pro-2.local>
2018-06-05 10:17:08 -0700

    Have TCP I/O components working. Need to enable concurrent input and ouptut via multithreading

--- test_ue4_controller.py
+++ test_ue4_controller.py
@@ -2,11 +2,11 @@ from datetime import datetime
 import socket
 
 TCP_IP = '127.0.0.1'
-TCP_IN = 5005
-TCP_OUT = 5006
+TCP_IN = 5000
+TCP_OUT = 5001
 BUFFER_SIZE = 1024
 
-SEC_BETWEEN_FIRE = 0.01
+SEC_BETWEEN_FIRE = 0.04
 SEC_OF_FIRING = 3
 
 
@@ -22,17 +22,28 @@ def main():
 
     :return:
     """
+    # wait 10 seconds
     now = datetime.now()
     later = now
 
+    while (later - now).total_seconds() < 10.0:
+        later = datetime.now()
+
+    # sending input to UE4
     in_port = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     in_port.connect((TCP_IP, TCP_IN))
 
+    # receiving output from UE4
     out_port = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    out_port.connect((TCP_IP, TCP_OUT))
+    out_port.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    out_port.bind((TCP_IP, TCP_OUT))
+    out_port.listen(1)
 
-    # wait 15 seconds, using delta timing
-    while (later - now).total_seconds() < 15.0:
+    # wait 5 seconds
+    now = datetime.now()
+    later = now
+
+    while (later - now).total_seconds() < 5.0:
         later = datetime.now()
 
     i = 0
@@ -41,11 +52,12 @@ def main():
         later = datetime.now()
 
         # print any output received
-        output = out_port.recv(BUFFER_SIZE)
-        if output is not '':
-            print(output)
+        out_connect, _ = out_port.accept()
+        out_data = out_connect.recv(BUFFER_SIZE)
+        if len(out_data) > 0:
+            print(out_data)
 
-        # type '0' 100 times / sec for 3 secs , using delta timing
+        # type '0' 25 times / sec for 3 secs, using delta timing
         if i < int(SEC_OF_FIRING / SEC_BETWEEN_FIRE) and (later - now).total_seconds() >= SEC_BETWEEN_FIRE:
             now = later
             i += 1
